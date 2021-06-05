09c34b868d1414729f1b9c683f029b32870cdbc7
AQUAMAN <78766821+shahrozxdr@users.noreply.github.com>
2021-06-05 14:16:35 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const db = require('quick.db');
 const { TOKEN, PREFIX, AVATARURL, BOTNAME, } = require(`./config.json`);
 const figlet = require("figlet");
 const client = new Client({ disableMentions: `` , partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
-client.login("ODEwMDkzMDEwMTgzOTEzNTAy.YCen6A.0nPCmCC4PnzwQP7-TKEkm24MmDw");
+client.login("");
 client.commands = new Collection();
 client.setMaxListeners(0);
 client.prefix = PREFIX;
